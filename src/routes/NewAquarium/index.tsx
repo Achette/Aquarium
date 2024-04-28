5d9efe427a615907d50aaaea7da4edfa322f8451
Achette <i.achette@live.com>
2024-04-28 12:53:09 -0300

    feat/pi-10: adiciona tela para cadastro de novo aquário

--- src/routes/NewAquarium/index.tsx
+++ src/routes/NewAquarium/index.tsx
@@ -1,20 +1,28 @@
-import { AquariumMaterial, PowerSupply } from '@/components'
+import { useMedia } from '@/hooks'
 import { AquariumFormats } from '@/components/Formats'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import {
+  AquariumDimensions,
+  AquariumMaterial,
+  AquariumPowerSupply,
+} from '@/components'
 import {
   Box,
   Button,
+  Flex,
   FormControl,
   Heading,
   Input,
   Text,
   VStack,
 } from '@chakra-ui/react'
-import { SubmitHandler, useForm } from 'react-hook-form'
 
 type NewAquariumProps = {
   name: string
 }
 export const NewAquarium = () => {
+  const { isMobile, isDesktop } = useMedia()
+
   const {
     register,
     handleSubmit,
@@ -25,7 +33,7 @@ export const NewAquarium = () => {
     console.log(data)
   }
   return (
-    <Box p="1.5rem 1rem 0">
+    <Box p="1.5rem 1rem 0" px={isDesktop ? '16%' : ''}>
       <Heading as="h2" color="blue.900" fontSize="1.75rem">
         Novo Aquário
       </Heading>
@@ -50,8 +58,41 @@ export const NewAquarium = () => {
 
           <AquariumFormats />
           <AquariumMaterial />
-          <PowerSupply />
-          {/*  <Button type="submit">Enviar</Button> */}
+          <AquariumPowerSupply />
+          <AquariumDimensions />
+
+          <Flex
+            flexDirection={isMobile ? 'column' : 'row'}
+            gap="1rem"
+            alignItems="center"
+            justifyContent="center"
+            mt="1.75rem"
+          >
+            <Button
+              type="submit"
+              w="19.5rem"
+              h="3rem"
+              borderRadius="3.75rem"
+              color="white.900"
+              bgColor="blue.900"
+              _hover={{ bgColor: 'blue.500' }}
+              fontSize="1rem"
+            >
+              Avançar
+            </Button>
+
+            <Button
+              type="submit"
+              w="19.5rem"
+              h="3rem"
+              colorScheme="blue"
+              variant="outline"
+              borderRadius="3.75rem"
+              fontSize="1rem"
+            >
+              Voltar
+            </Button>
+          </Flex>
         </FormControl>
       </VStack>
     </Box>
