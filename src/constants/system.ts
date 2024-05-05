5b13da363d0a5adb7828efd8f2271657a4dfd37f
Achette <i.achette@live.com>
2024-05-05 15:10:04 -0300

    refactor: altera nome dos tokens

--- src/constants/system.ts
+++ src/constants/system.ts
@@ -1,9 +1,10 @@
 /**
  * LOCALSTORAGE KEYS
  */
-export const TOKEN_KEY = 'com.dev.make_a_list/Token'
-export const USER_KEY = 'com.dev.make_a_list/User'
-export const USER_KEY_ID = 'com.dev.make_a_list/UserId'
+export const TOKEN_KEY = 'com.dev.aquarium/Token'
+export const USER_KEY = 'com.dev.aquarium/User'
+export const USER_KEY_ID = 'com.dev.aquarium/UserId'
+export const AQUARIUM_ID = 'com.dev.aquarium/id'
 
 /**
  * URL
