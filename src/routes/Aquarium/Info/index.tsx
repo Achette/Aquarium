0528f39a8b2e668c7ee67baca41dbbbf7c6f0710
Achette <i.achette@live.com>
2024-05-18 17:01:23 -0300

    chore: adiciona novas funcionalidades na tela de detalhes

--- src/routes/Aquarium/Info/index.tsx
+++ src/routes/Aquarium/Info/index.tsx
@@ -1,13 +1,33 @@
+import React from 'react'
+import { useMedia } from '@/hooks'
 import { Box } from '@chakra-ui/react'
+import { AquariumProps } from '@/models'
 import { useParams } from 'react-router-dom'
+import { AquariumServices } from '@/services/aquarium-services'
+import { AquariumDetailsTopBar, DetailsBox } from '@/components'
 
 export const AquariumInfo = () => {
+  const [aquariumData, setAquariumData] = React.useState<AquariumProps>()
+
   const { id } = useParams()
+  const { isDesktop } = useMedia()
+
+  const fetchAquarium = React.useCallback(async () => {
+    const response = await AquariumServices.getById(id!)
+    setAquariumData({ ...response })
+  }, [id])
+
+  React.useEffect(() => {
+    fetchAquarium()
+  }, [fetchAquarium])
 
   return (
-    <Box>
-        {id}
-        Essa é a tela de informações do aquário
+    <Box pt="1.5rem" px={isDesktop ? '16%' : '0.5rem'}>
+      <AquariumDetailsTopBar
+        format={aquariumData?.format}
+        name={aquariumData?.name}
+      />
+      <DetailsBox />
     </Box>
   )
 }
