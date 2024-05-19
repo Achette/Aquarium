7b08dcc628d9de81f2616e757f72e701a993a3c7
Achette <i.achette@live.com>
2024-05-18 22:59:06 -0300

    feat: adiciona novos states ao context do app

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -7,6 +7,12 @@ interface AppContextProps {
   setMaterial: React.Dispatch<SetStateAction<string>>
   powerSupply: string
   setPowerSupply: React.Dispatch<SetStateAction<string>>
+  thickness: number
+  setThickness: React.Dispatch<SetStateAction<number>>
+  height: number
+  setHeight: React.Dispatch<SetStateAction<number>>
+  volume: number
+  setVolume: React.Dispatch<SetStateAction<number>>
 }
 
 type AppProviderProps = {
@@ -21,6 +27,9 @@ export function AquariumProvider({ children }: AppProviderProps) {
   const [format, setFormat] = React.useState<string>('')
   const [material, setMaterial] = React.useState<string>('')
   const [powerSupply, setPowerSupply] = React.useState('')
+  const [thickness, setThickness] = React.useState(0)
+  const [height, setHeight] = React.useState(0)
+  const [volume, setVolume] = React.useState(0)
 
   return (
     <AquariumContext.Provider
@@ -31,6 +40,12 @@ export function AquariumProvider({ children }: AppProviderProps) {
         setMaterial,
         powerSupply,
         setPowerSupply,
+        thickness,
+        setThickness,
+        height,
+        setHeight,
+        volume,
+        setVolume,
       }}
     >
       {children}
