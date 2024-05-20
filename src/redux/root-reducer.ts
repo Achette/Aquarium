2f536109ccf9125793b8c5b7a6c379adcd46f095
Achette <i.achette@live.com>
2024-05-19 22:48:08 -0300

    feat: adiciona slice de pets ao root-reducer

--- src/redux/root-reducer.ts
+++ src/redux/root-reducer.ts
@@ -1,4 +1,5 @@
 import { combineReducers } from 'redux'
 import aquariumDetails from './reducers/aquariumDetails'
+import petsDetails from './reducers/petsDetails'
 
-export const rootReducer = combineReducers({aquariumDetails})
+export const rootReducer = combineReducers({ aquariumDetails, petsDetails })
