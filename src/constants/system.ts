513a4a469fe648a3e0ea4ca742885a7f4b91ba5e
Achette <i.achette@live.com>
2024-05-23 20:31:08 -0300

    feat: adiciona url deploy para backend

--- src/constants/system.ts
+++ src/constants/system.ts
@@ -1,13 +1,14 @@
 /**
+ * URL
+ */
+
+export const BASE_URL = 'https://fine-janeva-rauls2011-54d1adaa.koyeb.app'
+
+
+/**
  * LOCALSTORAGE KEYS
  */
 export const TOKEN_KEY = 'com.dev.aquarium/Token'
 export const USER_KEY = 'com.dev.aquarium/User'
 export const USER_KEY_ID = 'com.dev.aquarium/UserId'
 export const AQUARIUM_ID = 'com.dev.aquarium/id'
-
-/**
- * URL
- */
-
-export const BASE_URL = 'http://localhost:8080'
